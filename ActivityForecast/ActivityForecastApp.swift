c9ee40effb0f2745bd8858b5f46df983c23515f8
suj1t <thoratsujit@fabex3d.com>
2026-09-04 11:12:09 +0530

    Initial Commit

---
+++ ActivityForecast/ActivityForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityForecastApp.swift
+//  ActivityForecast
+//
+//  Created by Sujit Thorat on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ActivityForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
